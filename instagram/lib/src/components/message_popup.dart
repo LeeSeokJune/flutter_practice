f948b35ee7d63b60884dec05101ebf07c7c8adb3
LeeSeokJune <indians1720@gmail.com>
2022-03-18 20:42:11 +0900

    study

---
+++ instagram/lib/src/components/message_popup.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+class MessagePopUp extends StatelessWidget {
+  final String? title;
+  final String? message;
+  final Function()? okCallback;
+  final Function()? cancelCallback;
+  const MessagePopUp({
+    Key? key,
+    required this.title,
+    required this.message,
+    required this.okCallback,
+    this.cancelCallback,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.transparent,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Container(
+              width: Get.width * 0.7,
+              color: Colors.white,
+              padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 10),
+              child: Column(
+                children: [
+                  Text(
+                    title!,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                      color: Colors.black,
+                    ),
+                  ),
+                  Text(
+                    message!,
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Colors.black,
+                    ),
+                  ),
+                  SizedBox(height: 15),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      ElevatedButton(onPressed: okCallback, child: Text('확인')),
+                      SizedBox(width: 10),
+                      ElevatedButton(
+                        onPressed: cancelCallback,
+                        child: Text('취소'),
+                        style: ElevatedButton.styleFrom(primary: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
---
+++ instagram/lib/src/controller/bottom_nav_controller.dart
@@ -0,0 +1,60 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:instagram/src/pages/upload.dart';
+
+import '../components/message_popup.dart';
+
+enum PageName { HOME, SEARCH, UPLOAD, ACTIVITY, MYPAGE }
+
+class BottomNavController extends GetxController {
+  RxInt pageIndex = 0.obs;
+  List<int> bottomHistory = [0]; // 팝 이벤트
+
+  void changeBottomNav(int value, {bool hasGesture = true}) {
+    var page = PageName.values[value];
+    switch (page) {
+      case PageName.UPLOAD:
+        Get.to(() => const Upload());
+        break;
+      case PageName.HOME:
+      case PageName.SEARCH:
+      case PageName.ACTIVITY:
+      case PageName.MYPAGE:
+        _chagePage(value, hasGesture: hasGesture);
+        break;
+    }
+  }
+
+  void _chagePage(int value, {bool hasGesture = true}) {
+    pageIndex(value);
+    if (!hasGesture) return;
+    if (bottomHistory.contains(value)) {
+      bottomHistory.remove(value);
+    }
+    bottomHistory.add(value);
+  }
+
+  Future<bool> willPopAction() async {
+    if (bottomHistory.length == 1) {
+      showDialog(
+        context: Get.context!,
+        builder: (context) => MessagePopUp(
+          message: '종료하겠습니까?',
+          okCallback: () {
+            exit(0);
+          },
+          cancelCallback: Get.back,
+          title: '시스템',
+        ),
+      );
+      return true;
+    } else {
+      bottomHistory.removeLast();
+      var index = bottomHistory.last;
+      changeBottomNav(index, hasGesture: false);
+      return false;
+    }
+  }
+}
